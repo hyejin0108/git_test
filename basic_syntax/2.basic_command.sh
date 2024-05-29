26638647cfa22d06e6db3fc140b12ea1362aea2c
hyejin0108 <hyejin0018@gmail.com>
2024-05-29 16:31:36 +0900

    명령어들

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -9,3 +9,21 @@ git log
 git add .
 
 # git add 특정파일 : 
+
+# 수정사항 취소
+git checkout .
+# 추가파일 취소
+git clean --fdx
+# 수정 취소 동시에 
+git checkout . | git clean --fdx
+# git add 취소
+git reset
+# git commit 취소
+git reset HEAD^
+# git push 취소
+git revert
+
+# 버전별 비교 명령어 : diff
+# A를 기준으로 B가 어떤 변경이 일어났는지
+git diff A B
+git diff commitID1 commitID12
